8caa0dad77477698d44591d1bd38684775d1c88c
ZJ-99 <Swei.J@outlook.com>
2021-09-07 21:02:47 +0800

    OJ

--- Study/OJ.C
+++ Study/OJ.C
@@ -514,3 +514,21 @@
 //	printf("%d, %d", *(ptr1 - 1), *(ptr2 - 1));
 //}
 
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//	pa++; // 
+//	printf("%s\n", *pa);
+//}
+int main()
+{
+	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	char** cp[] = { c + 3,c + 2,c + 1,c };
+	char*** cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *-- * ++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+	return 0;
+}
