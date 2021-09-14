5c02b160faf5c67205c7efbb4deb59bedda4b886
ZJ-99 <Swei.J@outlook.com>
2021-09-14 19:50:30 +0800

    OJ

--- Study/OJ.C
+++ Study/OJ.C
@@ -702,3 +702,27 @@
 //	if (flag == 0)
 //		printf("do not have prime!");
 //}
+
+#include<string.h>
+#include<assert.h>
+char* my_strcat(char* dest, char* src)
+{
+	assert(dest && src);
+	char* ret = dest;
+	while (*dest)
+		dest++;
+	while (*dest++ = *src++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "abc";
+	char arr2[] = { 'd','e','f','\0' };
+	my_strcat(arr1, arr2);
+
+	printf("%s", arr1);
+	return 0;
+}
