21227dc71feddbbc049057865ed6c9de83de05b5
ZJ-99 <Swei.J@outlook.com>
2021-09-20 17:46:10 +0800

    OJ

--- Study/OJ.C
+++ Study/OJ.C
@@ -949,9 +949,44 @@
 //}
 //int main()
 //{
-//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr1[20] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr2[10] = { 0 };
 //
-//	my_memcpy(arr + 2, arr, 6);
+//	my_memcpy(arr1, arr2, 4*sizeof(arr1[0]));
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//		printf("%d ", arr1[i]);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<assert.h>
+//void* my_memmove(void* dest, const void* src, size_t count)
+//{
+//	void* ret = dest;
+//	assert(dest && src);
+//	if (dest < src)
+//	{
+//		while (count--)
+//		{
+//			*(char*)dest = *(char*)src;
+//			dest = (char*)dest + 1;
+//			src = (char*)src + 1;
+//		}
+//	}
+//	else
+//	{
+//		while (count--)
+//		{
+//			*((char*)dest + count) = *((char*)src + count);
+//		}
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	my_memmove(arr + 2, arr, 4 * sizeof(arr[0]));
 //	int i = 0;
 //	for (i = 0; i < 10; i++)
 //		printf("%d ", arr[i]);
