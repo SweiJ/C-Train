e462281f1e952f72add373ff5ca7aecf19562d86
ZJ-99 <Swei.J@outlook.com>
2021-09-24 20:09:29 +0800

    日常

---
+++ Study/Knowledge.c
@@ -0,0 +1,37 @@
+#define	_CRT_SECURE_NO_DEPRECATE 1
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 4;
+//	while (n--)
+//		printf("%d", --n);
+//}
+//int main()
+//{
+//	int x = 10, y = 10, i;
+//	for (i = 0; x > 8; y = ++i)
+//		printf("%d %d", x--, y);
+//}
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void GetMemory(char** p)
+{
+	*p = (char*)malloc(100);
+}
+void Test(void)
+{
+	char* str = NULL;
+	GetMemory(&str);
+	strcpy(str, "Hello world");
+	printf("%s", str);
+	free(str);
+	str = NULL;
+}
+int main()
+{
+	Test();
+	return 0;
+}
