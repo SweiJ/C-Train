bebbb6b4afa65dbfd35d5f59650727bd72cc2aae
ZJ-99 <Swei.J@outlook.com>
2021-09-27 19:05:56 +0800

    通讯录

--- Study/contact.c
+++ Study/contact.c
@@ -36,10 +36,10 @@ void AddContact(struct Contact* pc)
 void ShowContact(struct Contact* pc)
 {
 	int i = 0;
-	printf("%15s\t%5s\t%8s\t%15s\t%30s\n\n", "name", "age", "sex", "tele", "addr");
+	printf("%10s\t%5s\t%8s\t%15s\t%30s\n\n", "name", "age", "sex", "tele", "addr");
 	for (i = 0; i < pc->sz; i++)
 	{
-		printf("%15s\t%5d\t%8s\t%15s\t%30s\n",
+		printf("%10s\t%5d\t%8s\t%15s\t%30s\n",
 			pc->data[i].name,
 			pc->data[i].age,
 			pc->data[i].sex,
@@ -49,7 +49,7 @@ void ShowContact(struct Contact* pc)
 }
 
 
-int FindContactByName(struct Contact* pc, char name[])
+int FindContactByName(const struct Contact* pc, const char name[])
 {
 	int i = 0;
 	for (i = 0; i < pc->sz; i++)
@@ -64,6 +64,11 @@ int FindContactByName(struct Contact* pc, char name[])
 }
 void DelContact(struct Contact* pc)
 {
+	if (pc->sz == 0)
+	{
+		printf("通讯录为空，无法删除\n");
+		return;
+	}
 	char name[NAME_MAX] = { 0 };
 	printf("请输入要删除人的名字：>");
 	scanf("%s", name);
@@ -77,10 +82,58 @@ void DelContact(struct Contact* pc)
 	{
 		// 删除
 		int j = 0;
-		for (j = pos; j < ; j++)
+		for (j = pos; j < pc->sz - 1; j++)
 		{
-
+			pc->data[j] = pc->data[j + 1];
 		}
+		pc->sz--;
+		printf("删除成功\n");
 	}
+}
 
+void SearchContact(const struct Contact* pc)
+{
+	char name[NAME_MAX] = { 0 };
+	printf("输入要查找的名字：>");
+	scanf("%s", name);
+	int pos = FindContactByName(pc, name);
+	if (-1 == pos)
+	{
+		printf("查无此人！\n");
+	}
+	else
+	{
+		printf("%10s\t%5s\t%8s\t%15s\t%30s\n\n", "name", "age", "sex", "tele", "addr");
+		printf("%10s\t%5d\t%8s\t%15s\t%30s\n",
+			pc->data[pos].name,
+			pc->data[pos].age,
+			pc->data[pos].sex,
+			pc->data[pos].tele,
+			pc->data[pos].addr);
+	}
+}
+
+void ModifyContact(struct Contact* pc)
+{
+	char name[NAME_MAX] = { 0 };
+	printf("输入要修改的名字：>");
+	scanf("%s", name);
+	int pos = FindContactByName(pc, name);
+	if (-1 == pos)
+	{
+		printf("要修改的人不存在！\n");
+	}
+	else
+	{
+		printf("请输入新的名字：>");
+		scanf("%s", pc->data[pos].name);
+		printf("请输入新的年龄：>");
+		scanf("%d", &(pc->data[pos].age));
+		printf("请输入新的性别：>");
+		scanf("%s", pc->data[pos].sex);
+		printf("请输入新的电话：>");
+		scanf("%s", pc->data[pos].tele);
+		printf("请输入新的地址：>");
+		scanf("%s", pc->data[pos].addr);
+	}
 }
